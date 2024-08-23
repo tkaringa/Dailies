e00a810bd2519bd77138707f51cde11f98cf759d
tkaringa <111993596+tkaringa@users.noreply.github.com>
2024-08-22 22:59:55 -0500

    Add files via upload

---
+++ Day8/scripts/main.js
@@ -0,0 +1,15 @@
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomColor();
+}
+
+// Change color on click event
+document.body.addEventListener('click', changeBackgroundColor);
